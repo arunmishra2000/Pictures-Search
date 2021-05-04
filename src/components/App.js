6a59dd3efd7c4f3af2f79cf07ce35b313ecdbebb
arnmih200 <arunkumar.mishra2019@vitstudent.ac.in>
2021-05-04 10:34:13 +0530

    Ui Changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,20 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import axios from "axios";
 
+axios
+  .get("https://api.github.com/")
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    console.log("hello");
+  });
+
 class App extends React.Component {
   onSearchSubmit(term) {
     console.log(term);
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -15,6 +15,7 @@ class SearchBar extends React.Component {
             <label>Image Search</label>
             <input
               type="text"
+              value={this.state.term}
               onChange={(e) => this.setState({ term: e.target.value })}
             />
           </div>
