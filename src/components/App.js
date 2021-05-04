dd10d50adc56cbb756e75d24ec695a9e549e7fbc
arnmih200 <arunkumar.mishra2019@vitstudent.ac.in>
2021-05-04 13:46:12 +0530

    ABC

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,16 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: {
-          query: term,
-        },
-        headers: {
-          Authorization:
-            "Client-ID 7eXPGFjI8Rz_j_UJq4s53VrEfE0r2rDGB8dYRDFXXVc",
-        },
-      })
-      .then((response) => {
-        console.log(response);
-      });
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization: "Client-ID 7eXPGFjI8Rz_j_UJq4s53VrEfE0r2rDGB8dYRDFXXVc",
+      },
+    });
+    console.log(response.data.results);
   }
   render() {
     return (
